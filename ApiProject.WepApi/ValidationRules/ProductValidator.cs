40a573c26e7d442176d148b62b40bc075e20214c
Murat Tokak <tokakmurat01@gmail.com>
2025-09-27 22:35:45 +0300

    Fluentvalidation used

---
+++ ApiProject.WepApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,24 @@
+﻿using ApiProject.WepApi.Entities;
+using FluentValidation;
+
+namespace ApiProject.WepApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x=>x.Name)
+                .NotEmpty().WithMessage("Ürün adını boş geçmeyiniz")
+                .MinimumLength(2).WithMessage("En az 2 karekter veri girişi yazınız")
+                .MaximumLength(50).WithMessage("En fazla 50 kareter veri girişi yazınız");
+
+            RuleFor(x=>x.Price)
+                .NotEmpty().WithMessage("Ürün fiyatı boş geçilemez")
+                .LessThan(0).WithMessage("Ürün fiyatı 0 dan küçün olamaz")
+                .GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz");
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Ürün açıklaması boş geçilemez");
+
+        }
+    }
+}
