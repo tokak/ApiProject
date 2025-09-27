84b057b1f4a7c790f5ed162a390423a5dfae598d
Murat Tokak <tokakmurat01@gmail.com>
2025-09-27 21:24:06 +0300

    General Mapping ve Auto Mapper

---
+++ ApiProject.WepApi/Dtos/FeatureDtos/GetByIdFeatureDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProject.WepApi.Dtos.FeatureDtos
+{
+    public class GetByIdFeatureDto
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
