91e55cfe34fe94523e6688fefe084d5780472ef4
Murat Tokak <tokakmurat01@gmail.com>
2025-09-27 18:58:19 +0300

    Completing category API operations

---
+++ ApiProject.WepApi/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using ApiProject.WepApi.Context;
+using ApiProject.WepApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult List()
+        {
+            List<Category> values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori eklendi");
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme İşlemi Başarılı");
+        }
+        [HttpPut]
+        public IActionResult Update(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
