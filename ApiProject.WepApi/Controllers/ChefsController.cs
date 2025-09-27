952c0c0dcc34031ba76fa11240d3541f11358c42
Murat Tokak <tokakmurat01@gmail.com>
2025-09-27 19:25:23 +0300

    Contact api operation success

---
+++ ApiProject.WepApi/Controllers/ChefsController.cs
@@ -0,0 +1,48 @@
+﻿using ApiProject.WepApi.Context;
+using ApiProject.WepApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("GetChefList")]
+        public IActionResult List()
+        {
+            List<Chef> values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme İşlemi Başarılı");
+        }
+        [HttpPut]
+        public IActionResult Update(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
