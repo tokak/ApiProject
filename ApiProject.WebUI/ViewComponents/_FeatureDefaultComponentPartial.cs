7fc5bb7f84e356666d5ec4aa7a63560c05b6c09a
Murat Tokak <tokakmurat01@gmail.com>
2025-09-28 18:11:03 +0300

    Example data used

---
+++ ApiProject.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents
+{
+    public class _FeatureDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
