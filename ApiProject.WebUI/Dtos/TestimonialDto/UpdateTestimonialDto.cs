0d7c18f9b2aef5ceef4fc5eedcd7e4c2403953c9
Murat Tokak <tokakmurat01@gmail.com>
2025-09-29 19:35:31 +0300

    Testimonial crud operation completed.

---
+++ ApiProject.WebUI/Dtos/TestimonialDto/UpdateTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProject.WebUI.Dtos.TestimonialDto
+{
+    public class UpdateTestimonialDto
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
