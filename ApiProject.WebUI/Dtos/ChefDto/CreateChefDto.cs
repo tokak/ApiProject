4fb71d62518f97eeabaee06ebb6d53b94924250e
Murat Tokak <tokakmurat01@gmail.com>
2025-09-29 19:48:44 +0300

    Chef and yummy event opereation crud completed.

---
+++ ApiProject.WebUI/Dtos/ChefDto/CreateChefDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProject.WebUI.Dtos.ChefDto
+{
+    public class CreateChefDto
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ApiProject.WebUI/Dtos/YummyEventDto/UpdateYummyEventDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProject.WebUI.Dtos.YummyEventDto
+{
+    public class UpdateYummyEventDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+        public decimal Price { get; set; }
+    }
+}
