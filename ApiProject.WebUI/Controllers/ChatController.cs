d2b4c3a1fa9cb5207774dd9fc1265aa14d356daa
Murat Tokak <tokakmurat01@gmail.com>
2025-10-02 00:44:10 +0300

    Instant Chat Process via AI with SignalR

---
+++ ApiProject.WebUI/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult SendChatWithAI()
+        {
+            return View();
+        }
+    }
+}
--- ApiProject.WebUI/Program.cs
+++ ApiProject.WebUI/Program.cs
@@ -1,5 +1,12 @@
+using ApiProject.WebUI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
+builder.Services.AddSignalR();
+builder.Services.AddHttpClient("deepseek", c =>
+{
+    c.BaseAddress = new Uri("https://api.sambanova.ai/");
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,6 +20,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapHub<ChatHub>("/chathub");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
